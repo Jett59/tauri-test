2c8e4bd744fc6efa43dcabdcbd9a4ea11dacf5e3
Jett Thompson <jett.thompson.59@gmail.com>
2023-01-09 10:06:24 +1100

    add a template project with react and mui

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Button } from "@mui/material";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+import { Box, Container, Stack } from "@mui/system";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <Container>
+      <h1>Welcome to Tauri!</h1>
+
+      <Stack direction="row">
+        <Box>
+          <TextField
+            id="greet-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter a name..."
+          />
+        </Box>
+        <Box>
+          <Button onClick={() => greet()}>
+            Greet
+          </Button>
+        </Box>
+      </Stack>
+      <Typography>{greetMsg}</Typography>
+    </Container>
+  );
+}
+
+export default App;
